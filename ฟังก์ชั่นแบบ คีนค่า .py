cc913944a278cf07716c07b8e5bc3e9a6b6b6a46
worachote1 <68600257+worachote1@users.noreply.github.com>
2021-02-22 00:26:18 +0700

    Add files via upload

---
+++ ฟังก์ชั่นแบบ คีนค่า .py
@@ -0,0 +1,15 @@
+# function return ค่า
+
+def add(a,b) : # รับค่าเข้ามาทํางานแล้วส่งออกมา
+    return a+b #ฟังก์ชั่นนี้เก็บค่า a+b เอาไว้ สามารถนําไปใช้ในโปรแกรมหลักได้ 
+
+
+print(add(10,20))
+
+x = add(10,30)
+
+def showNumber() :
+    return 500
+
+a = showNumber()
+print("ตัวเลข = ",a)    
